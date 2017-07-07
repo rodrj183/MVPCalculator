f5196d6c724d7d359144090c20074ebb22735e0d
Joshua Rodriguez <joshua.rodriguez@globant.com>
2017-07-07 08:16:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/joshuarodriguez/mvpcalculator/mvp/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.example.joshuarodriguez.mvpcalculator.mvp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.joshuarodriguez.mvpcalculator.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
